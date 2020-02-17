a638686f5bcf16092e5ac75cacfef10aa3555e8d
stanislav <stanislav.elshaidt@saritasa.com>
2020-02-17 14:49:15 +0600

    improved gulp.js

--- wp-content/themes/astra-child/assets/gulpfile.js
+++ wp-content/themes/astra-child/assets/gulpfile.js
@@ -1,12 +1,12 @@
 const
     scssPath = './app/scss/*.scss',
+    phpPath = './../../**/*.php',
 
     { src, dest, watch } = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    //autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync').create();
-
+    autoprefixer = require('gulp-autoprefixer'),
+    bs = require('browser-sync').create();
 
 function scssTask(){
  return src(scssPath)
@@ -14,29 +14,26 @@ function scssTask(){
      .pipe(sass({
          outputStyle: 'compressed'
      }))
-     /*.pipe(autoprefixer({
-         browsers: ['last 2 versions'],
-         cascade: false
-     }))*/
+     .pipe(autoprefixer())
      .pipe(sourcemaps.write('.'))
      .pipe(dest('./dist'))
-     .pipe(browserSync.stream())
+     .pipe(bs.stream())
 }
 
 function watchTask(){
-    browserSync.init({
-            proxy: 'media/',
-            port: 8080
+    bs.init({
+        proxy: 'media/',
+        port: 8080
     });
     watch(scssPath).on('change', function(file){
         const date = new Date();
-        console.log(`${file} изменен в ${date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}:${date.getSeconds()}`)
+        console.log(`${file} изменен в ${date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}:${date.getSeconds()}`);
         scssTask()
-        browserSync.reload
-    })
-    watch('./../../**/*.php').on('change', function(){
-        browserSync.reload
+    });
+    watch(phpPath).on('change', function(file){
+        console.log(file);
+        bs.reload();
     })
 }
 
-exports.default = watchTask
+exports.default = watchTask;
